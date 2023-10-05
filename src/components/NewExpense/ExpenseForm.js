b85d402d5de7ea7c24001123f517a2fa49f1f834
Avdhesh616 <avdhesh.ku616@gmail.com>
2023-10-05 23:18:37 +0530

    ' ' '' '
    ''es/.cache/babel-loader/fc4927140571e2090775500b79a71bea6da836a5fe12977bf324164348002a84.json

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -28,12 +28,15 @@ const ExpenseForm = () => {
     }
 
     const submitHandler = (event) =>{
+
+        event.preventDefault();
         const expenseData = {
             title: enteredTitle,
             amount: enteredAmount,
             date: new Date(enteredDate)
         };
-    }
+        console.log(expenseData);
+    };
     return (<form onSubmit={submitHandler}>
         <div className='new-expense__controls'>
             <div className='new-expense__control'>
